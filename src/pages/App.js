dff0b3f4286b5ab0fcab9f125daa3bb2e8d371dc
Thamizhselvan <thamizhtechie@gmail.com>
2020-09-06 14:17:06 +1000

    removing dependecies

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,9 @@
 import withRoot from '../modules/withRoot';
 // --- Post bootstrap -----
 import React from 'react';
-import ProductCategories from '../modules/views/ProductCategories';
-import ProductSmokingHero from '../modules/views/ProductSmokingHero';
 import AppFooter from '../modules/views/AppFooter';
 import ProductHero from '../modules/views/ProductHero';
 import ProductValues from '../modules/views/ProductValues';
-import ProductHowItWorks from '../modules/views/ProductHowItWorks';
-import ProductCTA from '../modules/views/ProductCTA';
 import AppAppBar from '../modules/views/AppAppBar';
 
 function App() {
@@ -21,4 +17,3 @@ function App() {
   );
 }
 export default withRoot(App);
-// export default App;
