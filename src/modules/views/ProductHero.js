24b09d3591444f3c4c0fee4384c1629f49ab5b2e
Thamizhselvan <thamizhtechie@gmail.com>
2020-09-06 13:57:05 +1000

    prop explorer

---
+++ src/modules/views/ProductHero.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Countdown from "react-countdown";
+import Button from '../components/Button';
+import Typography from '../components/Typography';
+import ProductHeroLayout from './ProductHeroLayout';
+
+
+const backgroundImage =
+  'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400&q=80';
+
+const styles = (theme) => ({
+  background: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundColor: '#7fc7d9', // Average color of the background image.
+    backgroundPosition: 'center',
+  },
+  button: {
+    minWidth: 200,
+  },
+  h5: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(10),
+    },
+  },
+  more: {
+    marginTop: theme.spacing(2),
+  },
+  countdownContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  countdownItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '50px',
+    boxSizing: 'border-box',
+    padding: '10px 8px',
+    margin: '12px 5px',
+    textTransform: 'uppercase',
+    borderRadius: '8px',
+    backgroundColor: '#6fb1a8',
+    color: '#ffffff',
+  },
+  countdownCount: {
+    fontWeight: 'bolder',
+    fontSize: '22px',
+    display: 'flex',
+    alignSelf: 'center',
+  },
+});
+
+function ProductHero(props) {
+  const { classes } = props;
+
+  const Completionist = () => <span>You are good to go!</span>;
+
+  const renderer = ({  days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      // Render a complete state
+      return <Completionist />;
+    } else {
+      // Render a countdown
+      return (
+        <div className={classes.countdownContainer}>
+          <span className={classes.countdownItem}>
+            <span className={classes.countdownCount}>
+            {days} 
+            </span>
+            Days
+          </span>
+          <span className={classes.countdownItem}>
+            <span className={classes.countdownCount}>
+            {hours} 
+            </span>
+            Hrs
+          </span>
+          <span className={classes.countdownItem}>
+            <span className={classes.countdownCount}>
+            {minutes} 
+            </span>
+            Mins
+          </span>
+          <span className={classes.countdownItem}>
+            <span className={classes.countdownCount}>
+            {seconds} 
+            </span>
+            Secs
+          </span>
+        </div>
+
+      );
+    }
+  };
+
+  return (
+    <ProductHeroLayout backgroundClassName={classes.background}>
+      {/* Increase the network loading priority of the background image. */}
+      <img style={{ display: 'none' }} src={backgroundImage} alt="increase priority" />
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+        Find your next property
+      </Typography>
+      {/* <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
+        Enjoy secret offers up to 20% off on the leading real estates in Australia
+      </Typography> */}
+      <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
+        Countdown to the launch
+      </Typography>
+      <Countdown date={'2021-01-01T00:00:00'} renderer={renderer} />
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        className={classes.button}
+        component="a"
+        id="bt-popup"
+      >
+        Pre-Register
+      </Button>
+      {/* <Typography variant="body2" color="inherit" className={classes.more}>
+        Discover more
+      </Typography> */}
+    </ProductHeroLayout>
+  );
+}
+
+ProductHero.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProductHero);
---
+++ src/modules/views/ProductValues.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import Typography from '../components/Typography';
+import productValues1 from '../../images/productValues1.svg';
+import productValues2 from '../../images/productValues2.svg';
+import productValues3 from '../../images/productValues3.svg';
+import productCurvyLines from '../../images/productCurvyLines.png';
+
+
+
+const styles = (theme) => ({
+  root: {
+    display: 'flex',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.secondary.light,
+  },
+  container: {
+    marginTop: theme.spacing(15),
+    marginBottom: theme.spacing(30),
+    display: 'flex',
+    position: 'relative',
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: theme.spacing(0, 5),
+  },
+  image: {
+    height: 55,
+  },
+  title: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(5),
+  },
+  curvyLines: {
+    pointerEvents: 'none',
+    position: 'absolute',
+    top: -180,
+  },
+});
+
+function ProductValues(props) {
+  const { classes } = props;
+
+  return (
+    <section className={classes.root}>
+      <Container className={classes.container}>
+        <img
+          src={productCurvyLines}
+          className={classes.curvyLines}
+          alt="curvy lines"
+        />
+        <Grid container spacing={5}>
+          <Grid item xs={12} md={4}>
+            <div className={classes.item}>
+              <img
+                className={classes.image}
+                src={productValues1}
+                alt="suitcase"
+              />
+              <Typography variant="h6" className={classes.title}>
+                Multiple Projects
+              </Typography>
+              <Typography variant="h5">
+                {'You will get exclusive access to information about many projects, now choose your next property with a range of choices across the country.'}
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <div className={classes.item}>
+              <img
+                className={classes.image}
+                src={productValues2}
+                alt="graph"
+              />
+              <Typography variant="h6" className={classes.title}>
+                Trusted Professionals
+              </Typography>
+              <Typography variant="h5">
+              {'Whether you are buying, selling or renting with us, you can rest assured you are in the hands of trusted professionals who live, breathe and love everything about Real estate.'}
+
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <div className={classes.item}>
+              <img
+                className={classes.image}
+                src={productValues3}
+                alt="clock"
+              />
+              <Typography variant="h6" className={classes.title}>
+                Exclusive rates
+              </Typography>
+              <Typography variant="h5">
+                {'By registering with us, you will access specially negotiated rates in the real estate properties and also get special updates on the latest property news'}
+              </Typography>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
+  );
+}
+
+ProductValues.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProductValues);
